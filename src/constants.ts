11a4320d3fc5a0c51465f5452b6a26e08eebf29e
Mark Brocato <mbrocato@edg.io>
2023-07-07 16:21:47 +0300

    EDGSITES-217 Switch to sending RUM events directly to EQ

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const DEST_URL = 'https://rum.ingress.layer0.co/ingress/rum/v1'
+export const DEST_URL = 'https://rum.ingress.edgio.net'
 export const SEND_DELAY = 500
 export const CACHE_MANIFEST_TTL = 3600
 export const CACHE_MANIFEST_DATA_KEY = 'rum_cache_manifest'
